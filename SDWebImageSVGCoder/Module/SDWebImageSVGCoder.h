f59220e69d32a16fcbb18b69ada6879b222e8fbb
DreamPiggy <lizhuoli1126@126.com>
2019-11-14 15:04:54 +0800

    Add the support to decode SVG into the bitmap representation, which is used for some image processing need bitmap

--- SDWebImageSVGCoder/Module/SDWebImageSVGCoder.h
+++ SDWebImageSVGCoder/Module/SDWebImageSVGCoder.h
@@ -10,6 +10,7 @@
 #endif
 
 #import <SDWebImageSVGCoder/SDImageSVGCoder.h>
+#import <SDWebImageSVGCoder/SDWebImageSVGCoderDefine.h>
 
 FOUNDATION_EXPORT double SDWebImageSVGCoderVersionNumber;
 FOUNDATION_EXPORT const unsigned char SDWebImageSVGCoderVersionString[];
